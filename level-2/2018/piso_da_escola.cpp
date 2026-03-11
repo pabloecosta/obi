570aa81f1d1b25b0cd930c24ca7b73cb5c21bfd8
pablo costa <pabloemanuelgamespb@gmail.com>
2026-03-11 12:47:35 -0300

    Add problem description for Piso da Escola
    Add problem statement and details for Piso da Escola.

--- level-2/2018/piso_da_escola.cpp
+++ level-2/2018/piso_da_escola.cpp
@@ -1,3 +1,16 @@
+/*
+Problem: Piso da Escola / School Floor
+Contest: OBI 2018 - Level 2
+Topics: Geometry, Implementation
+Link: https://olimpiada.ic.unicamp.br/pratique/p2/2018/f1/piso/
+
+Idea: Given the dimensions of the floor (width and length), compute how many tiles of each type
+are needed to cover the entire floor.
+
+By analyzing the geometric structure of the grid, we can derive formulas for both tile types
+based on the floor dimensions.
+    This problem mainly practices geometric reasoning and algebraic manipulation.
+*/
 
 #include <iostream>
 
