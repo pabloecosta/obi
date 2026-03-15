2efbcf9ec29b693f24f3a0cbad6c5756c8b337ed
pablo costa <pabloemanuelgamespb@gmail.com>
2026-03-15 16:07:35 -0300

    Add tira-teima.cpp to check point inclusion

---
+++ level-junior/2012/tira-teima.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x, y;
+
+    cin >> x >> y;
+
+    if(x>=0 && x<=432 && y>=0 && y<=468){
+        cout << "dentro";
+    }
+    else{
+        cout << "fora";
+    }
+}
