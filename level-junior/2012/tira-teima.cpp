0059148827b6792b5eed90351095dc7a856b53d5
pablo costa <pabloemanuelgamespb@gmail.com>
2026-03-15 16:21:52 -0300

    Add problem description for Tira-Teima challenge
    Added problem statement and idea for the Tira-Teima challenge.

--- level-junior/2012/tira-teima.cpp
+++ level-junior/2012/tira-teima.cpp
@@ -1,3 +1,12 @@
+/*
+Problem: Tira-Teima / Instant Replay
+Contest: OBI 2012 - Level Junior
+Topics: Implementation, Conditionals
+Link: https://olimpiada.ic.unicamp.br/pratique/pj/2012/f2/tenis/
+
+Idea: Check whether the point (x, y) lies inside the rectangular region.
+If both coordinates are within the given bounds, the point is inside the rectangle.
+*/
 
 #include <iostream>
 
