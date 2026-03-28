dd0dbaa6977afdbfa6605f06aef3e2323094ae1f
pablo costa <pabloemanuelgamespb@gmail.com>
2026-03-28 11:41:46 -0300

    Update frequencia_na_aula.cpp

--- level-2/2012/frequencia_na_aula.cpp
+++ level-2/2012/frequencia_na_aula.cpp
@@ -1,15 +1,20 @@
 /*
 Problem: Frequência na Aula / School Frequency
 Contest: OBI 2012 - Level 2
-Topics: Implementation, Arrays
+Topics: Implementation, Arrays / Set (STL)
 Link: https://olimpiada.ic.unicamp.br/pratique/p2/2012/f1/frequencia/
 
-Idea:
+Idea Solution 1:
 Read the attendance list and mark each identifier that appears.
 Finally, count how many identifiers were marked, which corresponds to the
 number of distinct students present.
+
+Idea Solution 2:
+Create a set with all registration numbers
+and compute how many elements appear in the set.
 */
 
+//Solution 1
 #include <iostream>
 
 using namespace std;
@@ -35,4 +40,24 @@ int main(){
 
     cout << contador;
 
+}
+
+//Solution 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> x(n);
+    
+    for(int i = 0; i < n; i++){
+        cin >> x[i];
+    }
+
+    unordered_set<int> s(x.begin(), x.end());
+
+    cout << s.size() << endl;
 }
