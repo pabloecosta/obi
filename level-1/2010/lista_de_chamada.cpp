0b35cb22f58001d5f19c454b1d630e61a0ce657d
pablo costa <pabloemanuelgamespb@gmail.com>
2026-03-28 12:13:42 -0300

    Update lista_de_chamada.cpp

--- level-1/2010/lista_de_chamada.cpp
+++ level-1/2010/lista_de_chamada.cpp
@@ -1,4 +1,13 @@
+/*
+Problem: Lista de Chamada / Attendance List
+Contest: OBI 2010 - Level 1
+Topics: Set
+Link: https://olimpiada.ic.unicamp.br/pratique/p1/2010/f2/chamada/
 
+Idea: Insert the names into a set to automatically sort them.
+Then iterate through the set until reaching the k-th element,
+and output the corresponding name.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,13 +17,13 @@ int main(){
     int n, k, c=0;
     cin >> n >> k;
 
-    vector<string> nome(n);
+    set<string> s;
 
     for(int i = 0; i < n; i++){
-        cin >> nome[i];
+        string name;
+        cin >> name;
+        s.insert(name);
     }
-
-    set<string> s(nome.begin(), nome.end());
 
     for(auto p = s.begin(); p != s.end(); p++){
         c++;
