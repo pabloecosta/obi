ed13371bbf8ab03d0ab7d1830c39bd19f6fca1b6
pablo costa <pabloemanuelgamespb@gmail.com>
2026-03-29 12:15:47 -0300

    Update lampadas_do_hotel.cpp

--- level-senior/2016/lampadas_do_hotel.cpp
+++ level-senior/2016/lampadas_do_hotel.cpp
@@ -1,4 +1,13 @@
+/*
+Problem: Lâmpadas do Hotel / Hotel Bulbs
+Contest: OBI 2016 - Level Senior
+Topics: Implementation, Conditionals
+Link: https://olimpiada.ic.unicamp.br/pratique/ps/2016/f1/lampadas-hotel/
 
+Idea: Simulate the effect of each switch press on the lamps 
+and determine the minimum number of presses needed to transform 
+the current configuration into the desired one.
+*/
 
 #include <iostream>
 using namespace std;
