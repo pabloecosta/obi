9689d501ae03a356efc3187964fffcb33fa7dc0b
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-04 17:19:38 -0300

    Add problem description for Gincana challenge
    Added problem statement and idea for the Gincana problem.

--- level-junior/2011/gincana.cpp
+++ level-junior/2011/gincana.cpp
@@ -1,4 +1,12 @@
+/*
+Problem: Gincana 
+Contest: OBI 2011 - Level Junior
+Topics: DFS, Graphs
+Link: https://olimpiada.ic.unicamp.br/pratique/pj/2011/f2/gincana/
 
+Idea: Represent each student as a graph vertex and teams as connected components.
+Compute the maximum number of teams that can be made, that is, how many connected components there are in total.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
