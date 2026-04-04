d6ea6487cb5f9e2546d9e31f1504124dd16b0d95
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-04 17:12:00 -0300

    Add DFS implementation to count graph components
    Implement depth-first search to count connected components in a graph.

---
+++ level-junior/2011/gincana.cpp
@@ -0,0 +1,41 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> alunos;
+vector<bool> visitados;
+
+void dfs(int i){
+
+    visitados[i] = true;
+
+    for(int c : alunos[i]){
+        if(!visitados[c]) dfs(c);
+        visitados[c] = true;
+    }
+}
+
+int main(){
+
+    int n, m; cin >> n >> m;
+
+    alunos.resize(n+1);
+    visitados.resize(n+1);
+
+    for(int i = 0; i < m; i++){
+        int x, y; cin >> x >> y;
+        alunos[x].push_back(y);
+        alunos[y].push_back(x);
+    }
+
+    int c = 0;
+
+    for(int i = 1; i < n+1; i++){
+        if(!visitados[i]) dfs(i), c++;
+    }
+
+    cout << c << endl;
+    return 0;
+
+}
