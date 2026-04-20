ffbbf7660dcb25ba0c4645988bdd54dd0fa9d793
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-20 16:23:55 -0300

    Rename mdc function to gcd and update calls

--- level-junior/2016/gincana.cpp
+++ level-junior/2016/gincana.cpp
@@ -1,25 +1,31 @@
+/*
+Problem: Gincana
+Contest: OBI 2016 - Level Junior
+Topics: GCD, Implementation
+Link: https://olimpiada.ic.unicamp.br/pratique/pj/2016/f2/gincana/
 
+Idea: Given two integers n and m, find the greatest value x that is no bigger than m
+such as gcd(n, x) = 1.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 
-bool ok;
-
-ll mdc(ll a, ll b){
+ll gcd(ll a, ll b){
     if(a < b) swap(a, b);
     
     if(a%b==0) return b;
     
-    return mdc(b, a%b);
+    return gcd(b, a%b);
 }
 
 int main(){
     ll n, m; cin >> n >> m;
   
     ll x = m;
-    while(mdc(x, n)!=1) x--;
+    while(gcd(x, n)!=1) x--;
     cout << x << endl;
   
 }
