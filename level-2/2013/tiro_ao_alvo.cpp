c24cfbfa07b5c397210ba9f8e3d8dde792b107c4
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-20 19:57:08 -0300

    Add target shooting program in tiro_ao_alvo.cpp
    Implement a C++ program for target shooting calculations.

---
+++ level-2/2013/tiro_ao_alvo.cpp
@@ -0,0 +1,46 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+vector<ll> r;
+ll pontos, meio;
+
+bool dentro(ll x, ll y, ll r){
+    
+    if(x*x + y*y <= r*r) return true;
+    return false;
+    
+}
+
+int main(){
+    
+    ll c, t; cin >> c >> t;
+    
+    r.resize(c+1);
+    
+    for(int i = 1; i <= c; i++){
+        cin >> r[i];
+    }
+    
+    for(int i = 0; i < t; i++){
+        ll x, y; cin >> x >> y;
+        
+        ll ini = 1, fim = c;
+        
+        while(ini <= fim){
+            
+            meio = (fim + ini)/2;
+            
+            if(!dentro(x, y, r[meio])) ini = meio+1;
+            else fim = meio - 1;
+            
+        }
+        
+        pontos += c - ini + 1;
+    }
+    
+    cout << pontos << endl;
+}
