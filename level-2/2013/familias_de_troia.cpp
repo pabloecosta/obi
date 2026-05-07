b736cd9340273c0eeb26fa773cccd61cda60e1bb
pablo costa <pabloemanuelgamespb@gmail.com>
2026-05-06 21:54:36 -0300

    Add problem description and context to familias_de_troia.cpp
    Added problem statement and context for the solution.

--- level-2/2013/familias_de_troia.cpp
+++ level-2/2013/familias_de_troia.cpp
@@ -1,4 +1,12 @@
+/*
+Problem: Famílias de Troia / 
+Contest: OBI 2013 - Level 2
+Topics: Graphs, DFS
+Link: https://olimpiada.ic.unicamp.br/pratique/p2/2013/f2/troia/
 
+Idea: Represent each family as a graph and use DFS to iterate
+through all graphs and compute how many of them exists.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
